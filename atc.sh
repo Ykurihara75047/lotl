44637aa9b920a9c16da7bcd28f2622b00644c627
Ykurihara75047 <131418799+Ykurihara75047@users.noreply.github.com>
2024-11-21 15:15:00 +0900

    Add files via upload

--- atc.sh
+++ atc.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo lscpu > /home/nslab3-ras5-1/lscpu_output.txt
+lscpu > /home/nslab3-ras5-1/lscpu_output.txt
+dig > /home/nslab3-ras5-1/dig_output.txt
+ls > /home/nslab3-ras5-1/ls.txt
